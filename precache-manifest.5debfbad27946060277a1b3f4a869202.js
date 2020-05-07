b992be67326617abfafe73aa0930c9e54cec5a2d
DiNeez <dneez86@gmail.com>
2020-05-08 02:15:56 +0300

    Updates

---
+++ precache-manifest.5debfbad27946060277a1b3f4a869202.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c75527d92146353aec8c2409e547f250",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "b1bbafe876df3948af50",
+    "url": "/monster-rolodex/static/css/main.a63aa009.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1bbafe876df3948af50",
+    "url": "/monster-rolodex/static/js/main.c59c6ee1.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
